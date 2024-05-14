32b82f0df9aeaed184705b297fc36c0457b53753
Ecolio <lioneljouvet@gmail.com>
2024-05-14 14:14:44 +0200

    original commit

--- backend/apps/audio/main.py
+++ backend/apps/audio/main.py
@@ -151,7 +151,7 @@ async def speech(request: Request, user=Depends(get_verified_user)):
 
     except Exception as e:
         log.exception(e)
-        error_detail = "Open WebUI: Server Connection Error"
+        error_detail = "uLynks LLMs Apps: Server Connection Error"
         if r is not None:
             try:
                 res = r.json()
